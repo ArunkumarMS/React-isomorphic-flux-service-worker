d655e26be8f589fc9ef0e200c0f4a2603de82743
Arunkumar <arunkumar.shivanna@in.tesco.com>
2016-02-29 23:22:40 +0530

    routes added

--- components/Welcome.js
+++ components/Welcome.js
@@ -10,7 +10,18 @@ class Welcome extends React.Component{
 
 		return(
 
-<div> <h1>Welcome React isomorphic flux service-worker all in one app </h1> </div>
+<div> <h1>Welcome React isomorphic flux service-worker all in one app </h1> 
+<form action="/WeatherReport">  
+  <input className="submit" type="submit" value="Server side rendering demo">
+  </input>
+</form>
+<form action="/Serviceworke">  
+
+  <input className="submit" type="submit" value="Serviceworke  Demo">
+  </input>
+</form>
+
+</div>
 			);
 	}
 }
